ed04622cea4a463b226bafb20eb5d8f76e0f2de9
BT2983 <BT2983@att.com>
2018-07-24 07:41:16 -0600

    Adding naming micro-service code - scripts-2.
    Refactor DB scripts.
    
    Change-Id: I9a9a2f57435a73f7adb35c388d33dad1f9316e75
    Issue-ID: CCSDK-342
    Signed-off-by: BT2983 <BT2983@att.com>

--- ms/neng/src/main/resources/db/changelog/scripts/rel_18_10/03_ref_data_upd_dml.sql
+++
@@ -1,8 +0,0 @@
---liquibase formatted sql
---changeset ref_data_update:18_10.ref_data_update.sql
-
-INSERT INTO SERVICE_PARAMETER(NAME, VALUE, CREATED_BY) VALUES ('max_gen_attempt','50','Initial');
-
-delete from IDENTIFIER_MAP;
-
-INSERT INTO IDENTIFIER_MAP(POLICY_FN_NAME, JS_FN_NAME, CREATED_BY) VALUES ('sub_str','substr', 'Initial'); 
--- ms/neng/src/main/resources/db/changelog/scripts/rel_18_10/09_table_upd_ddl.sql
+++
@@ -1,4 +0,0 @@
---liquibase formatted sql
---changeset generate_name_ddl_update:18_10.table_update.sql
-
-alter table generated_name MODIFY COLUMN created_time DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL;; 
