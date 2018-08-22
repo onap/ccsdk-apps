653f403e13d4014b3f80fc8bf5302866110e2202
Timoney, Dan (dt5972) <dt5972@att.com>
2018-08-22 10:10:11 -0400

    Revert "Add VlantagApi Functional Component"
    This reverts commit 822c98964b0a4f7b4da5dbe79669e54011a0778d.
    This contains no jUnit test cases, bringing code coverage from 71% to 37%.
    
    Change-Id: If748dd23e9b4eddd227f01e0b33d2aa9bcb88102
    Issue-ID: CCSDK-475
    Signed-off-by: Timoney, Dan (dt5972) <dt5972@att.com>

--- ms/vlantag-api/src/main/docker/startService.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-app_args=-Dspring.config.location=${APP_CONFIG_HOME}
-echo "app_args ="${app_args}
-java -Djava.security.egd=file:/dev/./urandom  ${app_args} -Xms1024m -Xmx1024m  -jar  /app.jar
