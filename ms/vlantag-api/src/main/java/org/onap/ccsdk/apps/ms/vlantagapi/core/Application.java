9221702b86540d7fb44aa6185fef6f15602d7838
Timoney, Dan (dt5972) <dt5972@att.com>
2018-08-22 10:46:23 -0400

    Restore "Add VlantagApi Functional Component""
    This reverts previous revert for VlantagApi functional
    component, with correction to pom.xml which had
    prevented jUnit tests from being counted.
    
    Change-Id: I8cff5d04949fe8e110565fb7f68f939caba2eff8
    Signed-off-by: Timoney, Dan (dt5972) <dt5972@att.com>
    Issue-ID: CCSDK-475

---
+++ ms/vlantag-api/src/main/java/org/onap/ccsdk/apps/ms/vlantagapi/core/Application.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright © 2017-2018 AT&T Intellectual Property.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package org.onap.ccsdk.apps.ms.vlantagapi.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Application.java Purpose: Entry point for the micro-service -- it starts up the application.
+ * 
+ * @author Saurav Paira
+ * @version 1.0
+ */
+@SpringBootApplication(scanBasePackages={"org.onap.ccsdk.apps.ms.vlantagapi", "org.onap.ccsdk.sli.core", "org.onap.ccsdk.sli.adaptors"})
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
