cc982ab01c95fd86d0c24d98d88644e2598e77ef
Sandeep J <sandeejh@in.ibm.com>
2018-11-16 14:48:51 +0530

    added test case to AaiServiceImplTest
    to increase code coverage
    
    Issue-ID: CCSDK-552
    Change-Id: Ib89f25c39bf0878170da6997ca74c587c4c72535
    Signed-off-by: Sandeep J <sandeejh@in.ibm.com>

--- ms/neng/src/main/java/org/onap/ccsdk/apps/ms/neng/service/extinf/impl/AaiServiceImpl.java
+++ ms/neng/src/main/java/org/onap/ccsdk/apps/ms/neng/service/extinf/impl/AaiServiceImpl.java
@@ -4,6 +4,8 @@
  * ================================================================================
  * Copyright (C) 2018 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
+ * Modifications Copyright (C) 2018 IBM.
+ * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -125,4 +127,8 @@ public class AaiServiceImpl {
         }
         return this.restTemplate;
     }
+
+	public void setAaiProps(AaiProps aaiProps) {
+		this.aaiProps = aaiProps;
+	}
 }
