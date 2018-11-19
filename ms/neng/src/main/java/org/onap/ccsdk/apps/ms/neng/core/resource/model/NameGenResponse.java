46e3fce4b1c84a10628242d12d10249d84a466ed
ezhil <ezhrajam@in.ibm.com>
2018-11-19 21:00:52 +0530

    Fixed sonar issue in NameGenResponse
    Made variable private for the class
    Issue-ID: CCSDK-732
    Change-Id: Idc7e8cceab09a8f01516a0b5d0953a7f113a3145
    Signed-off-by: ezhil <ezhrajam@in.ibm.com>

--- ms/neng/src/main/java/org/onap/ccsdk/apps/ms/neng/core/resource/model/NameGenResponse.java
+++ ms/neng/src/main/java/org/onap/ccsdk/apps/ms/neng/core/resource/model/NameGenResponse.java
@@ -4,6 +4,8 @@
  * ================================================================================
  * Copyright (C) 2018 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
+ * Modifications Copyright (C) 2018 IBM.
+ * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -29,7 +31,7 @@ import java.util.Map;
  */
 public class NameGenResponse implements Serializable {
     private static final long serialVersionUID = -8039686696076337053L;
-    List<Map<String, String>> elements;
+    private static List<Map<String, String>> elements;
 
     public List<Map<String, String>> getElements() {
         return elements;
