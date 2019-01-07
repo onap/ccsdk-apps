3e0823cef59767ff6f4c9b69f408331250c54efa
Alexis de Talhouët <adetalhouet89@gmail.com>
2019-01-07 16:33:02 -0500

    Add support for blueprintprocessor runtime DB
    Made controllerblueprint db repository generic so implementation
    details can be re-used.
    With this generic approach, all required for blueprintprocessor to
    be able to have its own table and interact with them is to create
    their definition, and use them within the generic instances.
    
    Change-Id: I0539e414e4ff3b7a6edf0f6304d6cbc5e6eac404
    Issue-ID: CCSDK-664
    Signed-off-by: Alexis de Talhouët <adetalhouet89@gmail.com>

--- components/core/src/main/kotlin/org/onap/ccsdk/apps/controllerblueprints/core/config/BluePrintLoadConfiguration.kt
+++ components/core/src/main/kotlin/org/onap/ccsdk/apps/controllerblueprints/core/config/BluePrintLoadConfiguration.kt
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.onap.ccsdk.apps.controllerblueprints.service.load
+package org.onap.ccsdk.apps.controllerblueprints.core.config
 
 open class BluePrintLoadConfiguration {
 
