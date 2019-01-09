995540c8ba14b19f98fb3e7fee6b71c4b497b89c
Alexis de Talhouët <adetalhouet89@gmail.com>
2019-01-09 16:57:48 -0500

    Add support for workflow validation
    Change-Id: Ia85e1180e09e9d08a02de515b1cc4158c3bccd5c
    Issue-ID: CCSDK-717
    Signed-off-by: Alexis de Talhouët <adetalhouet89@gmail.com>

--- components/core/src/test/kotlin/org/onap/ccsdk/apps/controllerblueprints/core/validation/BluePrintValidatorServiceImplTest.kt
+++ components/core/src/test/kotlin/org/onap/ccsdk/apps/controllerblueprints/core/validation/BluePrintValidatorServiceImplTest.kt
@@ -16,28 +16,89 @@
 
 package org.onap.ccsdk.apps.controllerblueprints.core.validation
 
+import org.junit.Ignore
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.Mockito.*
+import org.onap.ccsdk.apps.controllerblueprints.core.BluePrintException
+import org.onap.ccsdk.apps.controllerblueprints.core.data.NodeTemplate
+import org.onap.ccsdk.apps.controllerblueprints.core.data.Step
+import org.onap.ccsdk.apps.controllerblueprints.core.data.Workflow
 import org.onap.ccsdk.apps.controllerblueprints.core.mock.MockBluePrintTypeValidatorService
+import org.onap.ccsdk.apps.controllerblueprints.core.service.BluePrintContext
 import org.onap.ccsdk.apps.controllerblueprints.core.utils.BluePrintMetadataUtils
+import kotlin.test.assertEquals
 import kotlin.test.assertTrue
+import kotlin.test.fail
 
+//@RunWith(MockitoJUnitRunner::class)
 class BluePrintValidatorServiceImplTest {
 
-    val blueprintBasePath: String = ("./../model-catalog/blueprint-model/starter-blueprint/baseconfiguration")
-
+    private val blueprintBasePath: String = ("./../model-catalog/blueprint-model/starter-blueprint/baseconfiguration")
+    private val bluePrintRuntime = BluePrintMetadataUtils.getBluePrintRuntime("1234", blueprintBasePath)
+    private val mockBluePrintTypeValidatorService = MockBluePrintTypeValidatorService()
+    private val defaultBluePrintValidatorService = BluePrintValidatorServiceImpl(mockBluePrintTypeValidatorService)
+    private val workflowValidator = BluePrintWorkflowValidatorImpl(mockBluePrintTypeValidatorService)
 
     @Test
     fun testValidateOfType() {
-        val bluePrintRuntime = BluePrintMetadataUtils.getBluePrintRuntime("1234", blueprintBasePath)
-
-        val mockBluePrintTypeValidatorService = MockBluePrintTypeValidatorService()
-
-        val defaultBluePrintValidatorService = BluePrintValidatorServiceImpl(mockBluePrintTypeValidatorService)
-
         val valid = defaultBluePrintValidatorService.validateBluePrints(bluePrintRuntime)
 
         assertTrue(valid, "failed in blueprint Validation")
-
     }
+
+    @Test
+    fun testValidateWorkflowFailToFoundNodeTemplate() {
+        val workflowName = "resource-assignment"
+
+        val step = Step()
+        step.target = "TestCaseFailNoNodeTemplate"
+        val workflow = Workflow()
+        workflow.steps = mutableMapOf("test" to step)
+        try {
+            workflowValidator.validate(bluePrintRuntime, workflowName, workflow)
+        } catch (e: BluePrintException) {
+            assertEquals("Failed to validate Workflow(resource-assignment)'s step(test)'s definition: couldn't find NodeTemplate(TestCaseFailNoNodeTemplate)", e.message)
+            return
+        }
+        fail()
+    }
+
+    @Test
+    @Ignore
+    fun testValidateWorkflowFailNodeTemplateNotDgGeneric() {
+        val workflowName = "resource-assignment"
+        val nodeTemplateName = "resource-assignment-process"
+
+        val nodeTemplate = mock(NodeTemplate::class.java)
+        doReturn("WRONG_TYPE").`when`(nodeTemplate).type
+
+        val blueprintContent = mock(BluePrintContext::class.java)
+        doReturn("").`when`(blueprintContent).nodeTemplateByName(nodeTemplateName)
+
+        val bluePrintRuntimeSpy = spy(bluePrintRuntime)
+        doReturn("").`when`(bluePrintRuntimeSpy).bluePrintContext()
+
+
+        val step = Step()
+        step.target = nodeTemplateName
+        val workflow = Workflow()
+        workflow.steps = mutableMapOf("test" to step)
+        try {
+            workflowValidator.validate(bluePrintRuntimeSpy, workflowName, workflow)
+        } catch (e: BluePrintException) {
+            assertEquals("Failed to validate Workflow(resource-assignment)'s step(test)'s definition: couldn't find NodeTemplate(TestCaseFailNoNodeTemplate)", e.message)
+            return
+        }
+        fail()
+    }
+
+    @Test
+    fun testValidateWorkflowSuccess() {
+        val workflowName = "resource-assignment"
+        workflowValidator.validate(bluePrintRuntime, workflowName, bluePrintRuntime.bluePrintContext().workflowByName(workflowName))
+    }
+
 }
 
