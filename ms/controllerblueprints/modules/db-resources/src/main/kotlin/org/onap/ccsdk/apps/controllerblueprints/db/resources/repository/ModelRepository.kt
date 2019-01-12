3bd4ba1b8d96edb9fc7410a0bb693ab662787d7e
Alexis de Talhouët <adetalhouet89@gmail.com>
2019-01-12 15:48:20 -0500

    Implement BluePrintCatalogService
    Change-Id: Ifcb0d730daec4da747d704c270b72b991e01f474
    Issue-ID: CCSDK-908
    Signed-off-by: Alexis de Talhouët <adetalhouet89@gmail.com>

--- ms/controllerblueprints/modules/db-resources/src/main/kotlin/org/onap/ccsdk/apps/controllerblueprints/db/resources/repository/ModelRepository.kt
+++ ms/controllerblueprints/modules/db-resources/src/main/kotlin/org/onap/ccsdk/apps/controllerblueprints/db/resources/repository/ModelRepository.kt
@@ -21,6 +21,7 @@ import org.jetbrains.annotations.NotNull
 import java.util.Optional
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.repository.NoRepositoryBean
+import javax.transaction.Transactional
 
 /**
  * @param <T> Model
@@ -42,23 +43,23 @@ interface ModelRepository<T> : JpaRepository<T, String> {
      *
      * @param artifactName artifactName
      * @param artifactVersion artifactVersion
-     * @return Optional<T>
+     * @return T?
      */
-    fun findByArtifactNameAndArtifactVersion(artifactName: String, artifactVersion: String): Optional<T>
+    fun findByArtifactNameAndArtifactVersion(artifactName: String, artifactVersion: String): T?
 
     /**
      * This is a findTopByArtifactNameOrderByArtifactIdDesc method
      *
      * @param artifactName artifactName
-     * @return Optional<T>
+     * @return T?
      */
-    fun findTopByArtifactNameOrderByArtifactVersionDesc(artifactName: String): Optional<T>
+    fun findTopByArtifactNameOrderByArtifactVersionDesc(artifactName: String): T?
 
     /**
      * This is a findTopByArtifactName method
      *
      * @param artifactName artifactName
-     * @return Optional<T>
+     * @return List<T>
      */
     fun findTopByArtifactName(artifactName: String): List<T>
 
@@ -66,7 +67,7 @@ interface ModelRepository<T> : JpaRepository<T, String> {
      * This is a findByTagsContainingIgnoreCase method
      *
      * @param tags tags
-     * @return Optional<ModelType>
+     * @return List<T>
      */
     fun findByTagsContainingIgnoreCase(tags: String): List<T>
 
@@ -76,6 +77,7 @@ interface ModelRepository<T> : JpaRepository<T, String> {
      * @param artifactName artifactName
      * @param artifactVersion artifactVersion
      */
+    @Transactional
     fun deleteByArtifactNameAndArtifactVersion(artifactName: String, artifactVersion: String)
 
     /**
