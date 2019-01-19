952b8db770578d3c4cfd58c465d913fa2daf6497
ezhil <ezhrajam@in.ibm.com>
2019-01-19 18:26:49 +0530

    Added Junit Testfile for PolicyFinderServiceDbImpl
    Change-Id: I126ae770b42807780c588e893c590b147ccb470c
    Issue-ID: CCSDK-933
    Signed-off-by: ezhil <ezhrajam@in.ibm.com>

---
+++ ms/neng/src/test/java/org/onap/ccsdk/apps/ms/neng/service/extinf/impl/PolicyFinderServiceDbImplTest.java
@@ -0,0 +1,43 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP : APPC
+ * ================================================================================
+ * Copyright (C) 2019 IBM.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ============LICENSE_END=========================================================
+ */
+package org.onap.ccsdk.apps.ms.neng.service.extinf.impl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.mockito.Mockito.*;
+import org.mockito.*;
+import org.mockito.runners.MockitoJUnitRunner;
+import static org.junit.Assert.assertNotNull;
+
+import org.onap.ccsdk.apps.ms.neng.core.resource.model.GetConfigResponse;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PolicyFinderServiceDbImplTest {
+    @InjectMocks
+    @Spy
+    PolicyFinderServiceDbImpl policyFinderServiceDb;
+
+    @Test(expected = NullPointerException.class)
+    public void testConfig() throws Exception {
+        doReturn(new GetConfigResponse()).when(policyFinderServiceDb).makeOutboundCall(Matchers.any(), Matchers.any());
+        assertNotNull(policyFinderServiceDb.getConfig("policy"));
+    }
+}
