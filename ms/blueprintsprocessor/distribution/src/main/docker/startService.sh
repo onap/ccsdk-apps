c2ddc152e21e8c6c0a4bf1072523e863f4340f1c
Steve Alphonse Siani <alphonse.steve.siani.djissitchi@ibm.com>
2019-01-21 11:55:48 -0500

    ControllerBlueprint blueprintsProcessor container
    Change-Id: I77df77757c276fca705091c1566c386f842269c4
    Issue-ID: CCSDK-949
    Signed-off-by: Steve Alphonse Siani <alphonse.steve.siani.djissitchi@ibm.com>

--- ms/blueprintsprocessor/distribution/src/main/docker/startService.sh
+++ ms/blueprintsprocessor/distribution/src/main/docker/startService.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-nodeName=ControllerBlueprints_1.0.0_$(cat /proc/self/cgroup | grep docker | sed s/\\//\\n/g | tail -1)
+nodeName=BlueprintsProcessor_1.0.0_$(cat /proc/self/cgroup | grep docker | sed s/\\//\\n/g | tail -1)
 
 echo "APP Config HOME : ${APP_CONFIG_HOME}"
 export APP_HOME=/opt/app/onap
