230f09a05a90149e15d270e5ce408c4a653cc823
Steve Alphonse Siani <alphonse.steve.siani.djissitchi@ibm.com>
2019-02-13 15:45:50 -0500

    Python library for Jython execution
    Change-Id: I0f0a66276a57a62b4441cc65c5bca4234a69817c
    Issue-ID: CCSDK-696
    Signed-off-by: Steve Alphonse Siani <alphonse.steve.siani.djissitchi@ibm.com>

--- components/scripts/python/ccsdk_blueprints/abstract_ra_processor.py
+++ components/scripts/python/ccsdk_blueprints/abstract_ra_processor.py
@@ -1,12 +1,36 @@
 from org.onap.ccsdk.apps.blueprintsprocessor.functions.resource.resolution.processor import ResourceAssignmentProcessor
+from blueprint_constants import *
+from java.lang import Exception
 
 
 class AbstractRAProcessor(ResourceAssignmentProcessor):
 
+    def __init__(self):
+        ResourceAssignmentProcessor.__init__(self)
+        self.status = PROPERTY_BLUEPRINT_STATUS_SUCCESS
+        self.error_message = None
+        self.ra_valid = False
+        self.value_to_resolve = None
+
     def process(self, execution_request):
-        print "Processing calling.."
-        return None
+        print "Processing calling from parent..."
+        try:
+            self.ra_valid = self.validate(execution_request)
+            self.value_to_resolve = execution_request.name
+        except Exception, e:
+            self.status = PROPERTY_BLUEPRINT_STATUS_FAILURE
+            self.error_message = "Get Running python scripting Failure :" + e.getMessage()
 
     def recover(self, runtime_exception, execution_request):
-        print "Recovering calling.."
+        print "Recovering calling from parent.."
         return None
+
+    @staticmethod
+    def validate(ra):
+        if ra.name is None or ra.name is None:
+            raise Exception("Failed getting value for template key (" + ra.name + ") and " +
+                            "dictionary key (" + ra.dictionaryName +
+                            ") of type (" + ra.type + ")")
+        else:
+            pass
+        return True
