0e7774cb6950679a11870b185a740c37c33a3b8b
Alexis de Talhouët <adetalhouet89@gmail.com>
2019-02-17 09:31:35 -0500

    Netconf resolution service
    Change-Id: I0346c88cd11136561426260a206bd7bf07f90dd3
    Issue-ID: CCSDK-1086
    Signed-off-by: Alexis de Talhouët <adetalhouet89@gmail.com>

--- components/scripts/python/ccsdk_netconf/common.py
+++ components/scripts/python/ccsdk_netconf/common.py
@@ -1,2 +1,11 @@
+class ResolutionHelper:
 
+  def __init__(self, component_function):
+    self.component_function = component_function
 
+  def resolve_and_generate_message_from_template_prefix(self, artifact_prefix):
+    return self.component_function.resolveAndGenerateMessage(artifact_prefix)
+
+  def resolve_and_generate_message(self, artifact_mapping, artifact_template):
+    return self.component_function.resolveAndGenerateMessage(artifact_mapping,
+                                                             artifact_template)
