746c20bf241a9e675b14f185fc9dda254eed4d9e
Chandan Ghosh <cghosh12@in.ibm.com>
2019-03-11 13:16:05 +0530

    Added new rest api for enrichment functionality
    Added new rest api for enrichment functionality
    
    Issue-ID: CCSDK-1067
    Change-Id: I2fcb906876728e37d49b0d4c9898535d223cdba6
    Signed-off-by: Chandan Ghosh <cghosh12@in.ibm.com>

--- ms/blueprintsprocessor/modules/inbounds/selfservice-api/src/main/kotlin/org/onap/ccsdk/apps/blueprintsprocessor/selfservice/api/ExecutionServiceController.kt
+++ ms/blueprintsprocessor/modules/inbounds/selfservice-api/src/main/kotlin/org/onap/ccsdk/apps/blueprintsprocessor/selfservice/api/ExecutionServiceController.kt
@@ -20,25 +20,39 @@ import io.swagger.annotations.ApiOperation
 import org.onap.ccsdk.apps.blueprintsprocessor.core.api.data.ACTION_MODE_ASYNC
 import org.onap.ccsdk.apps.blueprintsprocessor.core.api.data.ExecutionServiceInput
 import org.onap.ccsdk.apps.blueprintsprocessor.core.api.data.ExecutionServiceOutput
+import org.onap.ccsdk.apps.blueprintsprocessor.selfservice.api.utils.FileExtract
+import org.onap.ccsdk.apps.blueprintsprocessor.selfservice.api.utils.FileStorage
+import org.onap.ccsdk.apps.controllerblueprints.core.interfaces.BluePrintEnhancerService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.core.io.UrlResource
+import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.http.codec.multipart.FilePart
-import org.springframework.security.access.prepost.PreAuthorize
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.RequestPart
-import org.springframework.web.bind.annotation.ResponseBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.multipart.MultipartFile
 import reactor.core.publisher.Mono
+import java.nio.file.Paths
 
 @RestController
 @RequestMapping("/api/v1/execution-service")
-open class ExecutionServiceController {
+class ExecutionServiceController {
+
+    @Autowired
+    lateinit var fileStorage: FileStorage
+
+    @Autowired
+    private val bluePrintEnhancerService: BluePrintEnhancerService? = null
 
     @Autowired
     lateinit var executionServiceHandler: ExecutionServiceHandler
+
+    @Value("\${blueprintsprocessor.blueprintDeployPath}")
+    lateinit var deployPath: String
+
+    @Value("\${blueprintsprocessor.blueprintArchivePath}")
+    lateinit var archivePath: String
 
     @RequestMapping(path = ["/ping"], method = [RequestMethod.GET], produces = [MediaType.APPLICATION_JSON_VALUE])
     @ResponseBody
@@ -49,23 +63,38 @@ open class ExecutionServiceController {
     @PostMapping(path = ["/upload"], consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
     @ApiOperation(value = "Upload CBA", notes = "Takes a File and load it in the runtime database")
     @ResponseBody
-    @PreAuthorize("hasRole('USER')")
     fun upload(@RequestPart("file") parts: Mono<FilePart>): Mono<String> {
         return parts
-            .filter { it is FilePart }
-            .ofType(FilePart::class.java)
-            .flatMap(executionServiceHandler::upload)
+                .filter { it is FilePart }
+                .ofType(FilePart::class.java)
+                .flatMap(executionServiceHandler::upload)
     }
 
     @RequestMapping(path = ["/process"], method = [RequestMethod.POST], produces = [MediaType.APPLICATION_JSON_VALUE])
     @ApiOperation(value = "Resolve Resource Mappings",
-        notes = "Takes the blueprint information and process as per the payload")
+            notes = "Takes the blueprint information and process as per the payload")
     @ResponseBody
-    @PreAuthorize("hasRole('USER')")
     fun process(@RequestBody executionServiceInput: ExecutionServiceInput): ExecutionServiceOutput {
         if (executionServiceInput.actionIdentifiers.mode == ACTION_MODE_ASYNC) {
             throw IllegalStateException("Can't process async request through the REST endpoint. Use gRPC for async processing.")
         }
         return executionServiceHandler.processSync(executionServiceInput)
+    }
+
+    @PostMapping(path = ["/enrichment"], consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
+    @ApiOperation(value = "Enrichment of uploaded json",
+            notes = "Enrichment of uploaded json")
+    @ResponseBody
+    fun enrichment(@RequestBody file: MultipartFile): ResponseEntity<org.springframework.core.io.Resource> {
+        fileStorage.store(file);
+        FileExtract().extract(deployPath+file.originalFilename,deployPath)
+        val bluePrintContext = bluePrintEnhancerService!!.enhance(deployPath+file.originalFilename.replace(".zip",""), deployPath+System.currentTimeMillis()+"\\")
+        //after enrichment need to zip the file
+        val fileName:String=FileExtract().zip(deployPath+System.currentTimeMillis()+"\\");
+        //need to send the file
+        val resource:org.springframework.core.io.Resource = UrlResource(Paths.get(fileName).toUri())
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"")
+                .body(resource);
     }
 }
