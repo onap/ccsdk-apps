5cc6e0ecb267b4b0a9415352f98d60edb54b677b
ottero <rodrigo.ottero@est.tech>
2019-03-15 19:24:20 +0000

    Adding custom headers capability to REST client
    For YANG PATCH requests to ODL to work, they need to have a Content-
    type header of application/yang.patch+json and should not have Accept
    as application/json
    
    Current REST client inserts a default header to the requests with this
    content:
    
      Content-Type: application/json
      Accept: application/json
    
    The solution was to add the possibility of sending custom headers alon-
    gside the other parameters.
    
    Change-Id: I2cf0cd2ef7b87f4f5a246d427dffafe266cb33f7
    Issue-ID: CCSDK-926
    Signed-off-by: ottero <rodrigo.ottero@est.tech>

--- components/model-catalog/blueprint-model/test-blueprint/capability_restconf/Scripts/python/RestconfConfigure.py
+++ components/model-catalog/blueprint-model/test-blueprint/capability_restconf/Scripts/python/RestconfConfigure.py
@@ -16,23 +16,48 @@
 # SPDX-License-Identifier: Apache-2.0
 # ============LICENSE_END=========================================================
 
-
 from org.onap.ccsdk.apps.blueprintsprocessor.functions.restconf.executor import \
     RestconfComponentFunction
+from java.lang import Exception as JavaException
 
 
 class RestconfConfigure(RestconfComponentFunction):
 
-
     def process(self, execution_request):
-        # create instances of the needed objects
-        # retrieve any needed information not present on the request, like pnf ip
-        # retrieve the configlet
-        # send the configlet
-        # end
-        print("process", execution_request)
+        log = globals()["log"]
+        log.info("Started execution of process method")
+        try:
+            log.info("getting pnf id")
+            pnf_id = execution_request.payload.get("configure-request").get("configure-properties").get("pnf-id")
+            pnf_id = str(pnf_id).strip('\"')
+            log.info("pnf-id: {}", pnf_id)
+
+            log.info("defining mount payload")
+            mount_payload = self.resolveAndGenerateMessage("configure-mapping", "configure-template")
+            log.info("mount payload: \n {}", mount_payload)
+
+            # defining custom header
+            headers = {
+                "Content-Type": "application/xml"
+            }
+
+            log.info("will test sending put request")
+            url = "restconf/config/network-topology:network-topology/topology/topology-netconf/node/" + str(pnf_id)
+            log.info("trying to call url: {}", url)
+            web_client_service = self.restClientService("sdncodl")
+            result = web_client_service.exchangeResource("PUT", url, mount_payload, headers)
+            log.info("result: {}", result)
+            log.info("Ended execution of process method")
+
+        except JavaException, err:
+            log.error("Java Exception in the script", err)
+            raise err
+        except Exception, err:
+            log.error("Python Exception in the script", err)
+            raise err
 
 
-    def recover(self, runtime_exception, execution_request):
-        print("recover")
+def recover(self, runtime_exception, execution_request):
+        log = globals()["log"]
+        log.info("Recover method, no code to execute")
         return None
