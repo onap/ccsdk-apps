1c762996e529bf8a08e8863eaf4c0cab8b891451
anushadasari <danush10@in.ibm.com>
2019-07-26 20:07:00 +0530

    JUNIT-  VlantagApiException
    JUNIT for file  VlantagApiException.java
    
    Issue-ID: CCSDK-1545
    Change-Id: I9798ffd5c801c5970aae53ef55405dd09852fa69
    Signed-off-by: anushadasari <danush10@in.ibm.com>

---
+++ ms/vlantag-api/src/test/java/org/onap/ccsdk/apps/ms/vlantagapi/core/exception/VlantagApiExceptionTest.java
@@ -0,0 +1,30 @@
+package org.onap.ccsdk.apps.ms.vlantagapi.core.exception;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+
+class VlantagApiExceptionTest {
+
+	@Test
+	public void TestVlantagApiException() {
+		assertNotNull(new VlantagApiException());
+	
+	}
+	
+	@Test
+	public void VlantagApiExceptionTestString() {
+		assertNotNull(new VlantagApiException("JUnit Test"));
+	
+	}
+	
+	@Test
+	public void VlantagApiExceptionTestStringThrowable() {
+		assertNotNull(new VlantagApiException("JUnit Test", new Exception("JUnit Test")));
+	
+	}
+	
+
+}
