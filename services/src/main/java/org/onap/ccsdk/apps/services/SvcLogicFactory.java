f3430368211a5eb435a56a70006831255dcdba2e
Dan Timoney <dtimoney@att.com>
2020-06-09 12:42:46 -0400

    Export SvcLogicFactory as service
    Expose SvcLogicFactory bean as a service so it can be reused in other
    microservices.
    
    Change-Id: I55a6e08ad2a4fa81a91151a3b437514fab70c96a
    Issue-ID: CCSDK-2096
    Signed-off-by: Dan Timoney <dtimoney@att.com>

--- services/src/main/java/org/onap/ccsdk/apps/services/SvcLogicFactory.java
+++ services/src/main/java/org/onap/ccsdk/apps/services/SvcLogicFactory.java
@@ -18,7 +18,7 @@
  * ============LICENSE_END=========================================================
  */
 
-package org.onap.ccsdk.apps.ms.sliboot.core;
+package org.onap.ccsdk.apps.services;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -44,8 +44,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Service;
 
 @Configuration
+@Service
 public class SvcLogicFactory {
   private static final Logger log = LoggerFactory.getLogger(SvcLogicFactory.class);
 
