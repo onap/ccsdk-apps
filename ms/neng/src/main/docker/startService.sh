a8143e905217ac42ebfaf22edf9910fc538070b4
Fiete Ostkamp <Fiete.Ostkamp@telekom.de>
2025-03-31 12:02:53 +0200

    Add tracing for neng Updated parent version to released version 3.1.0
    Issue-ID: SDNC-1854
    Change-Id: I5675a1a0604daf3fd99e6d2a5e2e1735578035e6
    Signed-off-by: Fiete Ostkamp <Fiete.Ostkamp@telekom.de>

--- ms/neng/src/main/docker/startService.sh
+++ ms/neng/src/main/docker/startService.sh
@@ -7,9 +7,9 @@
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
 #  You may obtain a copy of the License at
-#  
+#
 #       http://www.apache.org/licenses/LICENSE-2.0
-#  
+#
 #  Unless required by applicable law or agreed to in writing, software
 #  distributed under the License is distributed on an "AS IS" BASIS,
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,19 +19,22 @@
 
 touch /app.jar
 APP_ARGS=-Dspring.profiles.active=${SPRING_PROFILE}
-APP_ARGS=${APP_ARGS}" -Dneng_db_user="${NENG_DB_USER}" -Dneng_db_pass="${NENG_DB_PASS}         
-APP_ARGS=${APP_ARGS}" -Dneng_db_url="${NENG_DB_URL}                                        
-APP_ARGS=${APP_ARGS}" -Dpol_client_auth="${POL_CLIENT_AUTH}                           
-APP_ARGS=${APP_ARGS}" -Dpol_basic_auth="${POL_BASIC_AUTH}  
-APP_ARGS=${APP_ARGS}" -Dpol_url="${POL_URL}                
-APP_ARGS=${APP_ARGS}" -Dpol_env="${POL_ENV}              
-APP_ARGS=${APP_ARGS}" -Dpol_req_id="${POL_REQ_ID}
-APP_ARGS=${APP_ARGS}" -Daai_cert_pass="${AAI_CERT_PASS}
-APP_ARGS=${APP_ARGS}" -Daai_cert_path="${AAI_CERT_PATH}
-APP_ARGS=${APP_ARGS}" -Daai_uri="${AAI_URI}
-APP_ARGS=${APP_ARGS}" -Daai_auth="${AAI_AUTH}
-APP_ARGS=${APP_ARGS}" -cp /opt/etc/config"
+APP_ARGS=${APP_ARGS} -Dneng_db_user=${NENG_DB_USER} -Dneng_db_pass=${NENG_DB_PASS}
+APP_ARGS=${APP_ARGS} -Dneng_db_url=${NENG_DB_URL}
+APP_ARGS=${APP_ARGS} -Dpol_client_auth=${POL_CLIENT_AUTH}
+APP_ARGS=${APP_ARGS} -Dpol_basic_auth=${POL_BASIC_AUTH}
+APP_ARGS=${APP_ARGS} -Dpol_url=${POL_URL}
+APP_ARGS=${APP_ARGS} -Dpol_env=${POL_ENV}
+APP_ARGS=${APP_ARGS} -Dpol_req_id=${POL_REQ_ID}
+APP_ARGS=${APP_ARGS} -Daai_cert_pass=${AAI_CERT_PASS}
+APP_ARGS=${APP_ARGS} -Daai_cert_path=${AAI_CERT_PATH}
+APP_ARGS=${APP_ARGS} -Daai_uri=${AAI_URI}
+APP_ARGS=${APP_ARGS} -Daai_auth=${AAI_AUTH}
+APP_ARGS=${APP_ARGS} -Dspring.application.name=sdnc-network-name-gen
+APP_ARGS=${APP_ARGS} -Dmanagement.tracing.enabled=${TRACING_ENABLED:false}
+APP_ARGS=${APP_ARGS} -Dmanagement.tracing.sampling.probability=${TRACING_SAMPLING_PROBABILITY}
+APP_ARGS=${APP_ARGS} -Dmanagement.zipkin.tracing.endpoint=${COLLECTOR_BASEURL:http://jaeger-collector.istio-config:9411}/api/v2/spans
+APP_ARGS=${APP_ARGS} -cp /opt/etc/config
 
 echo "APP_ARGS ="${APP_ARGS}
 java -Djava.security.egd=file:/dev/./urandom  ${APP_ARGS} -Xms1024m -Xmx1024m -jar /app.jar --spring.config.location=/opt/etc/config/ >(tee -a /tmp/app.out) 2> >(tee -a /tmp/app.err >&2)
-
