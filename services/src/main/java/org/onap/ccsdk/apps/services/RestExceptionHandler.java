218c71fb4b443a0d51b33d80489d08ba69e53d2c
Dan Timoney <dtimoney@att.com>
2026-03-31 15:07:22 -0400

    Upgrade to springboot 3.5
    Upgrade to springboot version 3.5
    
    Issue-ID: CCSDK-4191
    Change-Id: I2b83d1bb4578c3e1de5f8b1e573ca366a55259b9
    Signed-off-by: Dan Timoney <dtimoney@att.com>

--- services/src/main/java/org/onap/ccsdk/apps/services/RestExceptionHandler.java
+++ services/src/main/java/org/onap/ccsdk/apps/services/RestExceptionHandler.java
@@ -1,17 +1,14 @@
 package org.onap.ccsdk.apps.services;
 
-import org.jvnet.hk2.annotations.Service;
 import org.springframework.beans.ConversionNotSupportedException;
 import org.springframework.beans.TypeMismatchException;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.http.converter.HttpMessageNotWritableException;
-import org.springframework.validation.BindException;
 import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
@@ -19,15 +16,12 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingPathVariableException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.ServletRequestBindingException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.context.request.async.AsyncRequestTimeoutException;
 import org.springframework.web.multipart.support.MissingServletRequestPartException;
 import org.springframework.web.servlet.NoHandlerFoundException;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -95,11 +89,6 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     @Override
     protected ResponseEntity<Object> handleMissingServletRequestPart(MissingServletRequestPartException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
         return createResponseEntity(new RestProtocolError("bad-message", "Missing servlet request part", ex), status);
-    }
-
-    @Override
-    protected ResponseEntity<Object> handleBindException(BindException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
-        return createResponseEntity(new RestApplicationError("binding-error", "Missing servlet request part", ex), status);
     }
 
     @Override
